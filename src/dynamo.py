3a7b9237c5bb389f7c43a9681fc69f851f248a1e
David Feinblum <dvfeinblum@gmail.com>
2019-11-09 21:34:54 -0500

    Moved validation to decorator and added standings command

---
+++ src/dynamo.py
@@ -0,0 +1,62 @@
+import boto3
+from collections import OrderedDict
+from dynamodb_json import json_util as dyn_json
+from flask import jsonify, request
+from src.config import aws_cfg
+
+client = boto3.client("dynamodb")
+
+
+def parse_team_stats(stat_blob: dict) -> str:
+    """
+    Currently, individual team's stats look like this from Dynamo:
+
+    {'wins': 82, 'losses': 0}
+
+
+    we turn these into a nice human readable line.
+
+    :param stat_blob: dict containing a team's stats
+    :return: strified version of this data
+    """
+
+    return f"Wins: {stat_blob.get('wins')}\t Losses: {stat_blob.get('losses')}\n"
+
+
+def parse_all_stats(stat_blob: dict) -> str:
+    return "\n".join(
+        [f"*{team}*\n{parse_team_stats(stats)}" for team, stats in stat_blob.items()]
+    )
+
+
+def get_standings(req: request):
+    """
+    Returns league standings information. If a valid team name is provided,
+    that team's data will be highlighted.
+
+    :param req: request object from the slack
+    :return: response body containing standings info.
+    """
+    team = req.form.get("text").lower()
+    standings = dyn_json.loads(
+        client.get_item(
+            TableName=aws_cfg.get("dynamo", {}).get("bot_state_table"),
+            Key={"key": {"S": "teams"}},
+        ).get("Item")
+    )
+    # We don't need this where we're goin'
+    standings.pop("key")
+
+    ordered_standings = OrderedDict(
+        sorted(standings.items(), key=lambda kv: kv[1]["wins"], reverse=True)
+    )
+
+    if team in ordered_standings:
+        team_stats = ordered_standings.get(team)
+        text = f"Here are the rec league standings for the *{team}*:\n{parse_team_stats(team_stats)}"
+    else:
+        text = (
+            f"Here are the rec league standings:\n{parse_all_stats(ordered_standings)}"
+        )
+
+    return jsonify(response_type="in_channel", text=text)
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,19 @@
 from datetime import datetime
+from functools import wraps
 
 from flask import abort, request
 import hashlib
 import hmac
 import logging
 
-from src.config import slack_cfg, SIGNING_SECRET_KEY
+from src.config import slack_cfg
 
 VERSION_NUMBER = "v0"
 
 logger = logging.getLogger(__name__)
 
 
-def is_validate_request(req: request) -> bool:
+def _is_valid_request(req: request) -> None:
     """
     This function ensures that the request we received came from Slack.
     Details on the algorithm here can be found at:
@@ -31,6 +32,21 @@ def is_validate_request(req: request) -> bool:
     logger.debug("Request data:" + req_data)
     basestring = f"{VERSION_NUMBER}:{req_timestamp}:{req_data}".encode("utf-8")
     my_signature = (
-        "v0=" + hmac.new(slack_cfg.get(SIGNING_SECRET_KEY).encode("utf-8"), basestring, hashlib.sha256).hexdigest()
+        "v0="
+        + hmac.new(
+            slack_cfg.get("signing_secret").encode("utf-8"), basestring, hashlib.sha256,
+        ).hexdigest()
     )
-    return hmac.compare_digest(my_signature, req_signature)
+    if not hmac.compare_digest(my_signature, req_signature):
+        logging.error("Request is improperly signed.")
+        abort(403)
+
+
+# decorator for the above functionality
+def validate_request(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        _is_valid_request(request)
+        return func(*args, **kwargs)
+
+    return wrapper
